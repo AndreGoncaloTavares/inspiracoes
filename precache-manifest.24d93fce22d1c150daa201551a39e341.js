14c167e86d5a29027aa3a208550b1f42ee841107
Andre Tavares <andre.tavares@talkdesk.com>
2020-10-22 11:53:23 +0100

    Updates

---
+++ precache-manifest.24d93fce22d1c150daa201551a39e341.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1c4905d787c61514ccc6d4f7080e80",
+    "url": "/inspiracoes/index.html"
+  },
+  {
+    "revision": "002664e589c48a426751",
+    "url": "/inspiracoes/static/css/main.281e955e.chunk.css"
+  },
+  {
+    "revision": "f5365d0c1f098746c910",
+    "url": "/inspiracoes/static/js/2.75fb8072.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/inspiracoes/static/js/2.75fb8072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002664e589c48a426751",
+    "url": "/inspiracoes/static/js/main.79f9da51.chunk.js"
+  },
+  {
+    "revision": "180e3cc7655163c682e9",
+    "url": "/inspiracoes/static/js/runtime-main.c96244b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inspiracoes/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1e6ef912d57112171e07144d0ce55a4c",
+    "url": "/inspiracoes/static/media/sunset.1e6ef912.jpg"
+  }
+]);
